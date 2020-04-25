65c72409f07bd92feda9b92bcceb6951a1ea68af
AssanSav <assane.savadogo81@gmail.com>
2020-04-24 22:59:13 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
-export function manageFriends(state, action){
+let state = { friends: [] }
+let action = {
+    type: "ADD_FRIEND" || "REMOVE_FRIEND",
+    friend: {
+        name: "Chrome Boi",
+        hometown: "NYC",
+        id: 1
+    }
+}
+
+export function manageFriends(state, action) {
+    // debugger
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {
+                ...state,
+                friends: [
+                    ...state.friends,
+                    { name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id }
+                ]
+            }
+        case "REMOVE_FRIEND":
+            // let removeFriend = state.friends.find(friend => friend.id === action.friend.id)
+            return 
+        default: 
+            return state
+    }
 }
